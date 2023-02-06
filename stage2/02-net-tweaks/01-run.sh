bbeac27767a21a4aa24dfde7cc1590a7d41344e3
Zachary Schneider <hello@zacharyschneider.ca>
2023-02-06 10:25:31 -0800

    fix: predictive interface naming

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
-#install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
-install -v -m 600 files/eth0.network	"${ROOTFS_DIR}/etc/systemd/network/eth0.network"
+install -v -m 644 files/eth0.network	"${ROOTFS_DIR}/etc/systemd/network/eth0.network"
 
+# Disable "Predictive Device Naming" (normally done by raspi-config)
+on_chroot <<EOF
+ln -sf /dev/null /etc/systemd/network/99-default.link
+ln -sf /dev/null /etc/systemd/network/73-usb-net-by-mac.link
+EOF
+
+# Enable network services
 on_chroot <<EOF
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
