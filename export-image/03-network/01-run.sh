a515d9630db40e05651761661093e1b510f90616
Zachary Schneider <hello@zacharyschneider.ca>
2023-02-07 21:13:58 -0800

    Use DNS servers provided by DHCP only

--- export-image/03-network/01-run.sh
+++ export-image/03-network/01-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
-install -m 644 files/resolv.conf "${ROOTFS_DIR}/etc/"
+#install -m 644 files/resolv.conf "${ROOTFS_DIR}/etc/"
+
+on_chroot <<EOF
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+EOF
